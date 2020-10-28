c700e7f0e4ef35cf7040d92ae353009150283664
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-10-28 16:41:23 -0500

    updated cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080', 'https://beerez.netlify.app/'
+    origins 'localhost:8080', 'https://beerez.netlify.app'
 
     resource '*',
       headers: :any,
